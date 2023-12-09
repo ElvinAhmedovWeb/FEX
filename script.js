9446be121d7ed566ae793e0ff390648069b14cba
ElvinAhmedovWeb <135613886+ElvinAhmedovWeb@users.noreply.github.com>
2023-12-09 21:39:11 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Add your form submission logic here
+    
+    alert('Form submitted successfully!');
+});
